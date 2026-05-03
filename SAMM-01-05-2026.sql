578cd83458b4a6b5b2ab1274e980af05f391402c
Clovis Vieira <vieira.clovis@gmail.com>
2026-05-03 20:05:17 -0300

    Create SQL script for wait statistics analysis
    Add SQL script to analyze wait statistics and clear them.

---
+++ SAMM-01-05-2026.sql
@@ -0,0 +1,19 @@
+SELECT wait_type, wait_time_ms / 1000.0 AS wait_time_s,
+       100.0 * wait_time_ms / SUM(wait_time_ms) OVER() AS percentage
+FROM sys.dm_os_wait_stats
+WHERE wait_type NOT IN (
+    'SLEEP_TASK', 'BROKER_RECEIVE_WAITFOR', 'CHECKPOINT_QUEUE',
+    'DISPATCHER_QUEUE_SEMAPHORE', 'SOS_WORK_DISPATCHER', 
+    'CLR_AUTO_EVENT', 'DIRTY_PAGE_POLL', 'HADR_FILESTREAM_IOMGR_IOCOMPLETION',
+    'LAZYWRITER_SLEEP', 'XE_TIMER_EVENT', 'QDS_PERSIST_TASK_MAIN_LOOP_SLEEP'
+)
+ORDER BY wait_time_ms DESC;
+
+
+-- Zerar estatísticas
+DBCC SQLPERF ('sys.dm_os_wait_stats', CLEAR);
+
+-- Locked Pages?
+
+SELECT locked_page_allocations_kb 
+FROM sys.dm_os_process_memory;
